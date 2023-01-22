36d1570e37c36187d4778c7eb1f4d893325bdb31
az <110185929+yyx990805@users.noreply.github.com>
2023-01-22 16:36:12 +0100

    refactor: justify-around replace to tailwind

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 export function Footer() {
-  return (<div className="flex">
+  return (<div className="flex justify-around">
     <div className="col-4">Reach<br/>...</div>
     <div className="col-4">Contact us<br/>...</div>
     <div className="col-4">Powered by<br/>...</div>
