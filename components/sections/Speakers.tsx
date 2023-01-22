4b2431827fe7528e29d8d9e5421ca08a596b2e79
az <110185929+yyx990805@users.noreply.github.com>
2023-01-22 15:50:17 +0100

    fix: speakers todo

--- components/sections/Speakers.tsx
+++ components/sections/Speakers.tsx
@@ -1,19 +1,19 @@
 import { SpeakerShort } from "../Schedule/SpeakerShort";
 
 export function Speakers({ speakers = [{id: '', name: ''}].slice(0) }) {
-  speakers = /* TODO speakers || */ `
+  speakers = /* TODO loading speakers from some kind of API || */ `
     speaker1, jan kowalski
     speaker2, mick jagger
     speaker3, lenny kilmister
     speaker4, evan you
   `
-  .split('\n')
-  .filter(e => Boolean(e.trim()))
-  .map(function lineToSpeakerObject(e) {
-    const vals = e.split(',');
-    const entries = 'id,name'.split(',').map((f, id) => [f, vals[id]])
-    return Object.fromEntries(entries)
-  })
+    .split('\n')
+    .filter(e => Boolean(e.trim()))
+    .map(function lineToSpeakerObject(e) {
+      const vals = e.split(',');
+      const entries = 'id,name'.split(',').map((f, id) => [f, vals[id]])
+      return Object.fromEntries(entries)
+    })
   return (<>
     <h2>Speakers</h2>
     <div>
