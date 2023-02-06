ed45de7fc4c760dbe5b01de382ac0bfbdc9ee806
az <110185929+yyx990805@users.noreply.github.com>
2023-02-06 05:10:01 +0100

    feat: refresh speakers page if env RELOAD

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/sections/Hero";
 import  Speakers  from "@/components/sections/Speakers";
-import { buildStaticProps } from "@/hooks/useRemoteData";
+import { buildStaticProps, useData } from "@/hooks/useRemoteData";
 
 export const getStaticProps = buildStaticProps(['speakers', 'info']);
 
 export function SpeakersPage({ speakers, info }: any) {
   if (process.env.NEXT_PUBLIC_RELOAD) {
+    ({speakers, info} = useData({speakers, info}));
   }
   return (
     <section className="bg-gradient-to-b from-[#A64AC9] to-white">
