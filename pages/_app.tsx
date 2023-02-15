7f9ac8fe474aad99bb9c283b3a786369a4c9a531
Piotr Zientara <314zientara@gmail.com>
2023-02-15 10:48:12 +0100

    Add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,29 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Layout } from "@/components/layout/Layout";
+import type {AppProps} from "next/app";
+import {Layout} from "@/components/layout/Layout";
+import Script from 'next/script'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <section>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </section>
-  );
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-124334065-1"/>
+            <Script
+                id='google-analytics'
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-124334065-1', {
+page_path: window.location.pathname,
+});
+`,
+                }}
+            />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
 }
