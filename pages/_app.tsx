92b540470bca4880962775cfa04cd5fab0886ca8
Piotr Zientara <314zientara@gmail.com>
2023-02-15 10:58:49 +0100

    Fix ts issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type {AppProps} from "next/app";
 import {Layout} from "@/components/layout/Layout";
 import Script from 'next/script'
 
-export default function App({Component, pageProps}) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <>
             <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-124334065-1"/>
