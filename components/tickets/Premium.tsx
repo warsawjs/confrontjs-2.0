b09b3a6c04c42abcc90487aeca8e70313b25e3bd
Mehdi <www.mehdi76mesy@gmail.com>
2023-02-16 22:04:13 +0100

    Fixed ticket details and added speakers social medias

--- components/tickets/Premium.tsx
+++ components/tickets/Premium.tsx
@@ -4,13 +4,19 @@ import Link from "next/link";
 export default function Premium({ ticket }: any) {
   return (
     <div
-      className="w-[240px] h-[440px] lg:w-[312px] lg:h-[530px] flex flex-col justify-around py-4 px-14 items-center 
+      className="w-[240px] h-[440px] lg:w-[312px] lg:h-[530px] flex flex-col justify-center gap-5 py-4 px-5 items-center 
        text-center bg-white drop-shadow-xl shadow-[#392740] rounded-3xl text-black font-medium"
     >
-      <h3 className="font-bold text-xl lg:text-2xl text-primary uppercase">
+      <h3 className="font-bold text-2xl text-primary lg:text-3xl uppercase">
         {ticket.type}
       </h3>
-      <p>{ticket.title}</p>
+      <div className="flex flex-col items-center text-center gap-2 text-base lg:text-xl text-primary">
+        <p>{ticket.feature1}</p>
+        <p>{ticket.feature2}</p>
+        <p>{ticket.feature3}</p>
+        <p>{ticket.feature4}</p>
+        <p className="font-bold">{ticket.feature5}</p>
+      </div>
       <hr
         style={{
           width: "80%",
@@ -19,7 +25,7 @@ export default function Premium({ ticket }: any) {
           background: "#A64AC9",
         }}
       />
-      <h3 className="font-bold text-xl lg:text-2xl text-primary uppercase">
+      <h3 className="font-bold text-xl lg:text-2xl text-primary uppercase py-5">
         {ticket.price}
       </h3>
       <Link
