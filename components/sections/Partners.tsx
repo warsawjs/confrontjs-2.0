186576c096a835d35b10095eca2a87d5ac1815bc
Mehdi <www.mehdi76mesy@gmail.com>
2023-02-21 17:35:35 +0100

    Added gold and silver sponsors categories and a bit of styling

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -17,22 +17,22 @@ export default function Partners({ data }: Props) {
   return (
     <div className="flex flex-col items-center justify-center py-40 pt-20 lg:px-40">
       <div className="flex flex-row justify-center text-center">
-        <h1 className="font-bold text-xl text-black lg:text-4xl pb-10">
-          Partners
-        </h1>
+        <h2 className="font-bold text-xl text-black lg:text-4xl uppercase">
+          partners
+        </h2>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-center gap-20 pt-2 lg:pt-3">
+      <div className="grid grid-cols-1 lg:grid-cols-4 items-center justify-center gap-32 pt-40">
         {Object.entries(filteredObj).map(([key, value]) => {
           return (
-            <div className="w-72 h-40 flex items-center justify-center">
+            <div className="w-50 h-30 flex items-center justify-center rounded-lg overflow-hidden">
               <Link href={value} target="_blank" rel="noopener noreferrer">
                 <Image
                   src={`https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/partners/${key.slice(
                     0,
                     -1
                   )}.png`}
-                  width={200}
-                  height={200}
+                  width={150}
+                  height={150}
                   className=""
                   alt="partner avatar"
                 />
--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -6,12 +6,62 @@ import data from "../../data/data.json";
 export default function Sponsors() {
   return (
     <div className="flex flex-col items-center justify-center py-40 lg:px-40">
-      <div className="flex flex-row justify-center text-center">
-        <h1 className="font-bold text-xl text-black lg:text-4xl pb-10">
-          Sponsors
-        </h1>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-center gap-20 pt-4 lg:pt-5">
+      <div className="flex flex-col">
+        <div className="flex justify-center text-center">
+          <h2 className="font-bold text-xl text-white lg:text-4xl uppercase">
+            sponsors
+          </h2>
+        </div>
+        <div className="flex flex-col items-center gap-10 justify-center pt-10">
+          <div className="flex flex-col items-center justify-center pt-20 text-center">
+            <h1 className="font-bold text-lg text-black lg:text-3xl pb-10">
+              Gold sponsor
+            </h1>
+            <div className="flex items-center justify-center gap-20">
+              {data.sponsors.map((sponsor) => {
+                if (sponsor.type === "gold") {
+                  return (
+                    <div className="w-72 h-40 flex items-center justify-center rounded-xl overflow-hidden cursor-pointer opacity-60 hover:opacity-100 transition-all duration-300 ease-in-out">
+                      <Link href={sponsor.url}>
+                        <Image
+                          width={1000}
+                          height={500}
+                          src={`https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-${sponsor.name}.png`}
+                          alt={sponsor.name}
+                        />
+                      </Link>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center pt-20 text-center">
+            <h1 className="font-bold text-lg text-black lg:text-3xl pb-10">
+              Silver sponsors
+            </h1>
+            <div className="grid grid-cols-1 lg:grid-cols-4 items-center justify-center gap-20">
+              {data.sponsors.map((sponsor) => {
+                if (sponsor.type === "silver") {
+                  return (
+                    <div className="w-64 h-40 flex items-center justify-center rounded-xl overflow-hidden cursor-pointer opacity-60 hover:opacity-100 transition-all duration-300 ease-in-out">
+                      <Link href={sponsor.url}>
+                        <Image
+                          width={800}
+                          height={400}
+                          src={`https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-${sponsor.name}.png`}
+                          alt={sponsor.name}
+                        />
+                      </Link>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-center gap-20 pt-4 lg:pt-5">
         {data.sponsors.map((sponsor) => {
           return (
             <div className="w-72 h-40 flex items-center justify-center rounded-xl overflow-hidden cursor-pointer opacity-60 hover:opacity-100 transition-all duration-300 ease-in-out">
@@ -26,6 +76,7 @@ export default function Sponsors() {
             </div>
           );
         })}
+      </div> */}
       </div>
     </div>
   );
