02774f51b003fb15c58a8d91abf5b6401bcd7310
Piotr Zientara <314zientara@gmail.com>
2023-11-20 14:53:37 +0100

    Update organizers

--- components/sections/Organizers.tsx
+++ components/sections/Organizers.tsx
@@ -78,27 +78,26 @@ export function Organizers() {
                 className="w-full h-full"
                 width={1000}
                 height={1000}
-                src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/gabi-czucha.png"
+                src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/aleksandra-michalowska.jpg"
                 alt="avatar"
               />
             </div>
             <div className="flex flex-col">
               <h2 className="font-bold text-sm text-white lg:text-xl">
-                Gabi Czucha
+                Aleksandra Michałowska
               </h2>
               <p>Organizer</p>
             </div>
             <p className="w-56 lg:w-64">
-              Creative and open minded chaos manager. "Sky is the limit" they
-              say, but she checked it out during flight attendant career and
-              truly believes that the limit is somewhere further.
+              Head of Operations, creator of the safe space.
+              No victor believes in chance.
             </p>
             <div className="text-4xl text-black flex justify-center gap-2 py-2">
               <Link
                 target="_blank"
                 rel="noopener"
                 className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.linkedin.com/in/gabriela-czucha-483b6320b/"
+                href="https://www.linkedin.com/in/sasha-rust/"
               >
                 {<AiFillLinkedin />}
               </Link>
@@ -120,112 +119,6 @@ export function Organizers() {
               >
                 {<AiFillFacebook />}
               </Link> */}
-            </div>
-          </div>
-
-          <div className="flex flex-col items-center text-center gap-4">
-            <div className="w-36 lg:w-72 h-36 lg:h-72 rounded-3xl overflow-hidden drop-shadow-xl">
-              <Image
-                className="w-full h-full"
-                width={1000}
-                height={1000}
-                src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/lucy-wieliczko.png"
-                alt="avatar"
-              />
-            </div>
-            <div className="flex flex-col">
-              <h2 className="font-bold text-sm text-white lg:text-xl">
-                Lucy Wieliczko
-              </h2>
-              <p>Assistant Organizer</p>
-            </div>
-            <p className="w-56 lg:w-64">
-              Communications specialist. She has an eye for beauty and passion
-              to demonstrate it.
-            </p>
-            <div className="text-4xl text-black flex justify-center gap-2 py-2">
-              <Link
-                target="_blank"
-                rel="noopener"
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.linkedin.com/in/julia-wieliczko-2a0878264/"
-              >
-                {<AiFillLinkedin />}
-              </Link>
-              {/* <Link
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://twitter.com/confrontjs"
-              >
-                {<AiFillTwitterCircle />}
-              </Link> */}
-              <Link
-                target="_blank"
-                rel="noopener"
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.instagram.com/lucy__wieliczko"
-              >
-                {<AiFillInstagram />}
-              </Link>
-              <Link
-                target="_blank"
-                rel="noopener"
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.facebook.com/Art.is.my.whole.life"
-              >
-                {<AiFillFacebook />}
-              </Link>
-            </div>
-          </div>
-
-          <div className="flex flex-col items-center text-center gap-4">
-            <div className="w-36 lg:w-72 h-36 lg:h-72 rounded-3xl overflow-hidden drop-shadow-xl">
-              <Image
-                className="w-full h-full"
-                width={1000}
-                height={1000}
-                src="https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/marcell-dengi.png"
-                alt="avatar"
-              />
-            </div>
-            <div className="flex flex-col">
-              <h2 className="font-bold text-sm text-white lg:text-xl">
-                Marcell Dengi
-              </h2>
-              <p>Assistant Organizer</p>
-            </div>
-            <p className="w-56 lg:w-64">
-              International Businessman and entrepreneur. ConfrontJS Webapp
-              Project Manager
-            </p>
-            <div className="text-4xl text-black flex justify-center gap-2 py-2">
-              <Link
-                target="_blank"
-                rel="noopener"
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.linkedin.com/in/marcell-dengi-379752163/?originalSubdomain=hu"
-              >
-                {<AiFillLinkedin />}
-              </Link>
-              {/* <Link
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://twitter.com/confrontjs"
-              >
-                {<AiFillTwitterCircle />}
-              </Link> */}
-              {/* <Link
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.instagram.com/confrontjs.event"
-              >
-                {<AiFillInstagram />}
-              </Link> */}
-              <Link
-                target="_blank"
-                rel="noopener"
-                className="hover:text-secondary transition-all duration-300 ease-in-out"
-                href="https://www.facebook.com/marcell.dengi"
-              >
-                {<AiFillFacebook />}
-              </Link>
             </div>
           </div>
         </div>
