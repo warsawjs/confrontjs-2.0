032ec12727983314761e00e50b41b4d7313a90b5
Mehdi <www.mehdi76mesy@gmail.com>
2024-03-01 17:21:26 +0100

    Add link to warsawJS from logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ import WJSLogo from "../public/WJSLogo.png";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col lg:flex-row justify-around bg-tertiary text-white text-xs lg:text-sm">
+    <div className="flex flex-col justify-around text-xs text-white lg:flex-row bg-tertiary lg:text-sm">
       <div className="h-[350px] flex flex-col items-center justify-around py-20 text-center">
         <p>
           Help us to reach
@@ -21,51 +21,56 @@ export default function Footer() {
         </p>
         <Link
           href="https://drive.google.com/drive/folders/1fxIe9BftuFlWGIaAQU5Hho_XVLV2iRva"
-          className="text-sm uppercase px-6 py-4 rounded-xl border-2 font-bold hover:bg-secondary hover:text-black transition-all duration-300 ease-in-out"
+          className="px-6 py-4 text-sm font-bold uppercase transition-all duration-300 ease-in-out border-2 rounded-xl hover:bg-secondary hover:text-black"
         >
           DOWNLOAD PRESS KIT
         </Link>
       </div>
 
-      <div className="flex flex-col items-center justify-around lg:py-20 text-center">
+      <div className="flex flex-col items-center justify-around text-center lg:py-20">
         <div>
-          <h1 className="font-bold text-xl uppercase lg:text-3xl">CONTACT</h1>
+          <h1 className="text-xl font-bold uppercase lg:text-3xl">CONTACT</h1>
           <p>
             If you have any questions,
             <br />
             write email to us:
           </p>
           <Link
-            className="underline underline-offset-4 uppercase"
+            className="underline uppercase underline-offset-4"
             href="mailto:confrontjs@warsawjs.com?subject=custom subject"
           >
             CONFRONTJS@WARSAWJS.COM
           </Link>
-          <div className="text-4xl text-primary flex justify-center gap-4 py-6">
+          <div className="flex justify-center gap-4 py-6 text-4xl text-primary">
             <Link
-              className="hover:text-secondary transition-all duration-300 ease-in-out"
+              className="transition-all duration-300 ease-in-out hover:text-secondary"
               href="https://twitter.com/confrontjs"
             >
               {<AiFillTwitterCircle />}
             </Link>
             <Link
-              className="hover:text-secondary transition-all duration-300 ease-in-out"
+              className="transition-all duration-300 ease-in-out hover:text-secondary"
               href="https://www.instagram.com/confrontjs.event"
             >
               {<AiFillInstagram />}
             </Link>
             <Link
-              className="hover:text-secondary transition-all duration-300 ease-in-out"
+              className="transition-all duration-300 ease-in-out hover:text-secondary"
               href="https://www.facebook.com/confrontjs"
             >
               {<AiFillFacebook />}
             </Link>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center">
+        <div className="flex flex-col items-center text-center">
           <p>
             Copyright @ 2024 by:{" "}
-            <Link className="underline" href="https://warsawjs.com/">
+            <Link
+              className="underline"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://warsawjs.com/"
+            >
               WarsawJS
             </Link>
           </p>
@@ -111,8 +116,14 @@ export default function Footer() {
       </div>
 
       <div className="flex flex-col items-center justify-around py-20 text-center">
-        <h1 className="font-bold text-xl lg:text-3xl">Powered by:</h1>
-        <Image width={145} height={145} src={WJSLogo} alt="warsawJS logo" />
+        <h1 className="text-xl font-bold lg:text-3xl">Powered by:</h1>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://warsawjs.com/"
+        >
+          <Image width={145} height={145} src={WJSLogo} alt="warsawJS logo" />
+        </Link>
       </div>
     </div>
   );
